2285493acfb10fabf6d1384b06e12404c8610a2d
Yeabaman <yeabsiratesfayegg@gmail.com>
2022-04-11 13:34:09 -0700

    task 1,2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the alphabet in lower casei
+ * 
+ *Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+
+}
